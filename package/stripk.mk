5345f1380588666497bde6d9a5b21adc48e86168
Dustin L. Howett <dustin@howett.net>
2018-12-21 22:15:25 -0800

    Switch to imgutil

--- package/stripk.mk
+++ package/stripk.mk
@@ -1,7 +1,7 @@
 ifeq ($(_THEOS_PACKAGE_FORMAT_LOADED),)
 include $(THEOS_MAKE_PATH)/package/deb.mk
 
-_THEOS_DEB_HAS_STIRRUP_SIGNIMAGE = $(call __executable,signimage)
+_THEOS_DEB_HAS_STIRRUP_SIGNIMAGE = $(call __executable,imgutil)
 ifneq ($(_THEOS_DEB_HAS_STIRRUP_SIGNIMAGE),$(_THEOS_TRUE))
 internal-package-check::
 	@echo "$(MAKE) package requires stirrup signimage."; exit 1
@@ -11,7 +11,7 @@ ifeq ($(_THEOS_DEB_CAN_PACKAGE),$(_THEOS_TRUE)) # Control file found (or layout/
 override _THEOS_DEB_PACKAGE_FILENAME = $(THEOS_PACKAGE_DIR)/$(THEOS_PACKAGE_NAME)_$(_THEOS_INTERNAL_PACKAGE_VERSION)_$(THEOS_PACKAGE_ARCH).ipk
 override _THEOS_SIGNED_PACKAGE_FILENAME = $(THEOS_PACKAGE_DIR)/$(THEOS_PACKAGE_NAME)_$(_THEOS_INTERNAL_PACKAGE_VERSION)_$(THEOS_PACKAGE_ARCH).stripk
 internal-package::
-	$(ECHO_NOTHING)signimage -key=$(PACKAGE_SIGNING_KEY) -cert=$(PACKAGE_SIGNING_CERT) -mime=application/x-opkg -out="$(_THEOS_SIGNED_PACKAGE_FILENAME)" "$(_THEOS_DEB_PACKAGE_FILENAME)"$(ECHO_END)
+	$(ECHO_NOTHING)imgutil sign -key=$(PACKAGE_SIGNING_KEY) -cert=$(PACKAGE_SIGNING_CERT) -mime=application/x-opkg -out="$(_THEOS_SIGNED_PACKAGE_FILENAME)" "$(_THEOS_DEB_PACKAGE_FILENAME)"$(ECHO_END)
 	$(ECHO_NOTHING)rm "$(_THEOS_DEB_PACKAGE_FILENAME)"$(ECHO_END)
 
 # This variable is used in package.mk
